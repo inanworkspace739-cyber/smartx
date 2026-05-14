e6e3031d7f5d30bcb506595ecaaf72cdd7f4f3ae
inanworkspace739-cyber <inanworkspace739@gmail.com>
2026-05-14 19:15:21 +0000

    initial commit

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    // Basic test placeholder for IPTV Player
-    expect(1 + 1, equals(2));
-  });
-}
